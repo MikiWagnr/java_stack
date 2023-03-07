75d0b5995bc25a54afc8bb4ed4407a1a9dfa25cb
MikiWagnr <miguelmontilla96@gmail.com>
2023-03-07 16:57:45 -0500

    initial comit

---
+++ fundamentals/greetings!/Test.java
@@ -0,0 +1,9 @@
+//Every file name must be the same as the class name.
+//every file Is a class
+public class Test {
+    //The file or class with the exact method signature below is always the launching point of the application you cannot run Java programs without it
+    public static void main(String[] args) {
+        //for now, all code goes here
+        System.out.println("hello world!");
+    }
+}
---
+++ fundamentals/myfirstjavaprogram/MyInfo.java
@@ -0,0 +1,5 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Miguel (Miki)\nI am 26 years old\nMy hometown is Brookly, NY ");
+    }
+}
